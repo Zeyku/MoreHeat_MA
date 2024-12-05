6fa9f71634e81686b3754db9b692ee6257c99b3f
Zeyku <matthiasx100@gmail.com>
2024-12-06 00:17:13 +0100

    added sceneswitcher, TimeManager and Timer

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TimeManager : MonoBehaviour
+{
+    public TextMeshProUGUI timer;
+    public AudioSource audioSource;
+    private float elapsedTime = 0.0f;
+    private float nextEventTime = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        int hours = Mathf.FloorToInt(elapsedTime / 3600);
+        int minutes = Mathf.FloorToInt(elapsedTime % 3600 / 60); // (elapsedTime % 3600)?
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+
+        timer.text = $"{hours:D2}:{minutes:D2}:{seconds:D2}";
+
+        if (elapsedTime >= nextEventTime)
+        {
+            TriggerEvent();
+            nextEventTime += 5f;
+        }
+    }
+
+    void TriggerEvent()
+    {
+        if(audioSource != null)
+        {
+            Debug.Log("90 seconds have passed");
+            audioSource.Play();
+        }
+        else
+        {
+            Debug.Log("No audio source attached to TimeManager");
+        }
+        
+    }
+}
